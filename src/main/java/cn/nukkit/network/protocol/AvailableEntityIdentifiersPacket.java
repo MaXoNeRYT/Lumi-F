2df5af0505acc2a50618034eefbf6a58f8cb0da0
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-11-29 19:11:16 +0300

    refactor: gamedata

--- src/main/java/cn/nukkit/network/protocol/AvailableEntityIdentifiersPacket.java
+++ src/main/java/cn/nukkit/network/protocol/AvailableEntityIdentifiersPacket.java
@@ -12,22 +12,14 @@ public class AvailableEntityIdentifiersPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.AVAILABLE_ENTITY_IDENTIFIERS_PACKET;
 
-    public static final byte[] NBT419;
-    public static final byte[] NBT440;
-    public static final byte[] NBT527;
-    public static final byte[] NBT544;
     public static final byte[] TAG; //582
 
     static {
-        NBT419 = loadEntityIdentifiers(419);
-        NBT440 = loadEntityIdentifiers(440);
-        NBT527 = loadEntityIdentifiers(527);
-        NBT544 = loadEntityIdentifiers(544);
         TAG = loadEntityIdentifiers(582);
     }
 
     private static byte[] loadEntityIdentifiers(int protocol) {
-        try (InputStream stream = Nukkit.class.getClassLoader().getResourceAsStream("entity_identifiers_" + protocol + ".dat")) {
+        try (InputStream stream = Nukkit.class.getClassLoader().getResourceAsStream("gamedata/entity/entity_identifiers_" + protocol + ".dat")) {
             return ByteStreams.toByteArray(stream);
         } catch (Exception e) {
             throw new AssertionError("Error whilst loading entity identifiers " + protocol, e);
