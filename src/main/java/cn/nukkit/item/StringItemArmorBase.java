1fdc5f1d4414d19ff0a3903d6faaca486bb773ce
SocialMoods <nikelev01@gmail.com>
2025-12-19 14:27:02 +0300

    refactor: use better annotations

--- src/main/java/cn/nukkit/item/StringItemArmorBase.java
+++ src/main/java/cn/nukkit/item/StringItemArmorBase.java
@@ -2,8 +2,7 @@ package cn.nukkit.item;
 
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class StringItemArmorBase extends ItemArmor implements StringItem {
 
